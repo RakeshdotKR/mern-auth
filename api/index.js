425c5886ed059cbc3d4f6537f61702ffe7e18a8c
RakeshdotKR <rakeshkrpdr@gmail.com>
2024-03-22 17:43:30 +0530

    Establish connection to DB

--- api/index.js
+++ api/index.js
@@ -1,4 +1,19 @@
+dotenv.config();
 import express from "express"; //Add this line in package.json "description": "","type":"module",
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+
+// console.log("Mongo Uri: ",process.env.MONGOURI);
+
+
+mongoose
+.connect(process.env.MONGOURI, { useNewUrlParser: true, useUnifiedTopology: true })
+.then(()=>{
+    console.log("Successfully connected to MongoDB");
+})
+.catch((err)=>{
+    console.log(err);
+});
 
 const app = express();
 
