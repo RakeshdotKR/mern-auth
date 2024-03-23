7383f55e06e2742ef5de317715e1d4f81c6c833c
RakeshdotKR <rakeshkrpdr@gmail.com>
2024-03-23 13:39:53 +0530

    create signin API route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,19 +1,40 @@
 import User from "../models/user.model.js";
-import bcryptjs from 'bcryptjs';
+import bcryptjs from "bcryptjs";
+import { errorhandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
-export const signup = async(req,res,next)=>{
-    console.log(req.body); //app.use(express.json()); to be added in index.js
+export const signup = async (req, res, next) => {
+  console.log(req.body); //app.use(express.json()); to be added in index.js
 
-    const {username,email,password} = req.body;
-    const hashPassword = bcryptjs.hashSync(password,10);
-    const newUser = new User({username,email,password:hashPassword});
-    
-    try{
-        await newUser.save();
-        res.status(201).json({message:'User created successfully'});
-    }
-    catch(error){
-        // res.status(500).json(error.message);
-        next(error);
-    }
+  const { username, email, password } = req.body;
+  const hashPassword = bcryptjs.hashSync(password, 10);
+  const newUser = new User({ username, email, password: hashPassword });
+
+  try {
+    await newUser.save();
+    res.status(201).json({ message: "User created successfully" });
+  } catch (error) {
+    // res.status(500).json(error.message);
+    next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) return next(errorhandler(404, "User not found"));
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) return next(errorhandler(401, "Wrong credentials"));
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const { password: hashedPassword, ...rest } = validUser._doc; //_doc: destructure
+    const expiryDate = new Date(Date.now() + 3600000); // 1 hour
+    res
+      .cookie("access_token", token, { httpOnly: true, expires: expiryDate })
+      .status(200)
+      //.json(validUser) //gives password as part of the response so we need to avoid this
+      .json(rest);
+  } catch (error) {
+    next(error);
+  }
 };
